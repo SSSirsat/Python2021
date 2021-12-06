f8288e669cc899338f31361445c38bebd8fdbcc1
shubahmsirsat <shubaham3221@gamil.com>
2021-12-06 20:58:09 +0530

    Version 2.0

---
+++ Polindrome.py
@@ -0,0 +1,11 @@
+def pol(num):
+    x1 = num[::-1]
+    if x1 == num:
+        print("Polindrome")
+    else:
+        print("Not a Polindrome")
+
+print("Shubham",pol("Shubham"))
+print("Madam",pol("Madam"))
+print("madam",pol("madam"))
+print("151",pol('151'))
